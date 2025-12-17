1743610866f0373b80f587a8a6e45c4abe504733
jaimedgp <jaimedgp@predictia.es>
2025-12-17 15:04:39 +0100

    Check if paths directories in config exists

--- pyseasonal/utils/config.py
+++ pyseasonal/utils/config.py
@@ -3,6 +3,21 @@ import os
 import yaml
 
 
+def _check_paths_exist(paths_dict: dict):
+    """Check if all paths in the dictionary exist"""
+    for key, path in paths_dict.items():
+        if "filename" in key:
+            continue
+
+        if key == "home" and path == "":
+            continue
+
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"Path for '{key}' does not exist: {key}: '{path}'")
+
+    return True
+
+
 def load_config(config_path):
     """Load configuration from YAML file"""
     print('The path of the configuration file is '+str(config_path))
@@ -13,8 +28,14 @@ def load_config(config_path):
     gcm_store = os.getenv('GCM_STORE', 'lustre')
     if gcm_store in config['paths']:
         paths = config['paths'][gcm_store]
-        config['paths'] = paths
     else:
         raise ValueError('Unknown entry for <gcm_store> !')
 
+    if _check_paths_exist(paths):
+        config['paths'] = paths
+
     return config
+
+
+if __name__ == '__main__':
+    load_config('config/config_for_pred2tercile_operational_medcof_jaimedgp.yaml')
